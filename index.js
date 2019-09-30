19b047c2a71217cca18c11db807a7d6ccb4ce542
Jordan <jordandoan@hotmail.com>
2019-09-30 10:17:49 -0700

    Added all routes for MVP

--- index.js
+++ index.js
@@ -4,4 +4,30 @@ const express = require('express'),
 const server = express();
 
 const port = 5000;
+
+server.get('/', (req,res) => {
+  res.send("Hey! You're on the / route.");
+})
+
+server.get('/api/users', (req,res) => {
+  res.send("hi");
+});
+
+server.post('/api/users', (req,res) => {
+  res.send("hi");
+});
+
+server.get('/api/users/:id', (req,res) => {
+  res.send("hi");
+});
+
+server.put('/api/users/:id', (req,res) => {
+  res.send("hi");
+});
+
+server.delete('/api/users/:id', (req,res) => {
+  res.send("hi");
+});
+
+
 server.listen(port, () => console.log(`Listening on port ${port}`))
